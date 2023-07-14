e2e57f6a81f573c42edf0b733189ded0039250f6
punit <punit.lakhwani@zignallabs.com>
2023-07-14 16:20:38 +0530

    solved sed issue

--- changeValue.sh
+++ changeValue.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-sed "s/client-pod-name/$1/g" temp.yml > deploy-test.yml
-sed "s/client-service-name/$2/g" temp.yml > deploy-test.yml
+sed "s/client-pod-name/$1/g" temp.yml > clinet-pod.yml
+sed "s/client-service-name/$2/g" clinet-pod.yml > deploy-test.yml
